1b1b86971a1a7d1f5b48b0e7aa28c8c038297bd8
Johannes Millan <johannes.millan@gmail.com>
2020-12-18 11:34:10 +0100

    feat(calendar): display tasks

--- src/app/features/calendar/calendar.component.ts
+++ src/app/features/calendar/calendar.component.ts
@@ -1,5 +1,9 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { CalendarOptions } from '@fullcalendar/angular';
+import { ChangeDetectionStrategy, Component, ViewChild } from '@angular/core';
+import { CalendarOptions, EventClickArg, FullCalendarComponent } from '@fullcalendar/angular';
+import { ScheduledTaskService } from '../tasks/scheduled-task.service';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { EventInput, EventSourceFunc, EventSourceInput, Identity } from '@fullcalendar/common';
 
 @Component({
   // apparently calendar does not work, so we add a prefix
@@ -9,11 +13,57 @@ import { CalendarOptions } from '@fullcalendar/angular';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CalendarComponent {
+  @ViewChild('calendar') calendarEl?: FullCalendarComponent;
 
-  calOptions: CalendarOptions = {
-    // initialView: 'daygrid'
+  private DEFAULT_CAL_OPTS: CalendarOptions = {
+    editable: true,
+    eventClick: (calEvent: EventClickArg) => {
+      console.log(calEvent);
+      // this.openDialog(calEvent);
+    },
+    events: [],
+    // dateClick: this.handleDateClick.bind(this), // bind is important!
+    // initialView: 'daygrid',
+    // events: [{
+    //   title: 'Asd',
+    //   start: new Date(),
+    //   allDay: true,
+    //   backgroundColor: 'red',
+    //   // end: new Date()
+    //   // display: 'string | null;',
+    //   // startEditable: 'boolean | null;',
+    //   // durationEditable: 'boolean | null;',
+    //   // constraints: 'Constraint[];',
+    //   // overlap: 'boolean | null;',
+    //   // allows: 'AllowFunc[];',
+    //   // backgroundColor: 'string;',
+    //   // borderColor: 'string;',
+    //   // textColor: 'string;',
+    //   // classNames: 'string[];',
+    // }],
   };
 
-  constructor() {
+  calOptions$: Observable<CalendarOptions> = this._scheduledTaskService.allScheduledTasks$.pipe(
+    map((tasks): CalendarOptions => {
+      const events: EventInput[] = tasks.map((task) => ({
+        title: task.title,
+        start: task.reminderData.remindAt,
+        editable: true,
+        startEditable: true,
+      }));
+      return {
+        ...this.DEFAULT_CAL_OPTS,
+        events,
+      };
+    })
+  );
+
+  constructor(
+    private _scheduledTaskService: ScheduledTaskService,
+  ) {
+  }
+
+  private handleDateClick(arg: any) {
+    alert('date click! ' + arg.dateStr);
   }
 }
